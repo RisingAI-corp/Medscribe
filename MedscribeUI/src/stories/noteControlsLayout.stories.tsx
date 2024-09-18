2c543ddc7b7cbebdd48620dd75c46d0c2ea60bd4
satyashah <88096065+satyashah@users.noreply.github.com>
2024-09-17 20:52:55 -0400

    add padding to stories

--- MedscribeUI/src/stories/noteControlsLayout.stories.tsx
+++ MedscribeUI/src/stories/noteControlsLayout.stories.tsx
@@ -5,11 +5,12 @@ import '@mantine/core/styles.css';
 
 const WIDTH = '300px';
 const HEIGHT = '500px';
+const PADDING = '10px';
 
 const NoteControlsWrapper = ({ isStatus, defaultHover }) => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
-      <div style={{ width: WIDTH, height: HEIGHT, border: '1px dotted red' }}>
+      <div style={{ width: WIDTH, height: HEIGHT, border: '1px dotted red', padding: PADDING }}>
         <MantineProvider>
           <NoteControlsLayout isStatus={isStatus} defaultHover={defaultHover} />
         </MantineProvider>
